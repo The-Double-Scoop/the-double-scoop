f31f4dad5e9d2b1fbd325c4e844090420e622bd9
Indira Mariya <indira.mariya@gmail.com>
2026-08-20 22:02:36 -0700

    fix asset loading for header

--- web-app/src/lib/components/Nav.svelte
+++ web-app/src/lib/components/Nav.svelte
@@ -1,8 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div class="pt-6">
-	<a href="/" class="block pb-4">
+	<a href="{base}/" class="block pb-4">
 		<!-- svelte-ignore a11y-img-redundant-alt -->
 		<img
-			src="/logo.png"
+			src="{base}/logo.png"
 			alt="Stylized version of The Double Scoop"
 			width="300"
 			height="30"
@@ -13,20 +17,20 @@
 	<div class="text-center border border-black mb-5 shadow-[0_3px_3px_rgba(0,0,0,0.3)]">
 		<div class="px-4 py-2 font-display font-semibold text-black text-base">
 			<nav class="container mx-auto flex justify-evenly items-center">
-				<a class="nav-link px-4 hover:text-red-500" href="/">
-					<img src="/logo1.png" alt="Home" class="w-6 h-6 mr-1 inline" />
+				<a class="nav-link px-4 hover:text-red-500" href="{base}/">
+					<img src="{base}/logo1.png" alt="Home" class="w-6 h-6 mr-1 inline" />
 					<span>HOME</span>
 				</a>
-				<a class="nav-link px-4 hover:text-red-500" href="/learn">
-					<img src="/logo2.png" alt="Learn" class="w-4 h-6 mr-1 inline" />
+				<a class="nav-link px-4 hover:text-red-500" href="{base}/learn">
+					<img src="{base}/logo2.png" alt="Learn" class="w-4 h-6 mr-1 inline" />
 					<span>LEARN</span>
 				</a>
-				<a class="nav-link px-4 hover:text-red-500" href="/stats">
-					<img src="/logo3.png" alt="Stats" class="w-6 h-6 mr-1 inline" />
+				<a class="nav-link px-4 hover:text-red-500" href="{base}/stats">
+					<img src="{base}/logo3.png" alt="Stats" class="w-6 h-6 mr-1 inline" />
 					<span>STATS</span>
 				</a>
-				<a class="nav-link px-4 hover:text-red-500" href="/contact">
-					<img src="/logo4.png" alt="Contact" class="w-6 h-6 mr-1 inline" />
+				<a class="nav-link px-4 hover:text-red-500" href="{base}/contact">
+					<img src="{base}/logo4.png" alt="Contact" class="w-6 h-6 mr-1 inline" />
 					<span>CONTACT</span>
 				</a>
 			</nav>
